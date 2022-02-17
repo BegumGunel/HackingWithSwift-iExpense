79ba56d868c6f7dca9e1eb86b8432bb9984dcbbc
Begum Gunel <begumgunel@gmail.com>
2022-02-17 11:57:04 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Begüm Günel on 17.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
